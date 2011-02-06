aea89fdf199f18e29396a75983f4f9df9fbc0759
Ulrich VACHON <u.vachon@gmail.com>
2011-02-06 21:28:45 +0100

    Added entries reader and dependecies

--- src/main/java/org/uva/training/Main.java
+++ src/main/java/org/uva/training/Main.java
@@ -1,11 +1,104 @@
 package org.uva.training;
 
-public class Main {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Collection;
 
-   public Main() {
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.uva.training.entity.Item;
+import org.uva.training.io.reader.EntryReader;
+import org.uva.training.utils.RawEntry;
+import org.uva.training.utils.RawEntryHandler;
+
+import com.google.common.base.Function;
+import com.google.common.base.Predicates;
+import com.google.common.collect.Collections2;
+
+/**
+ * Entry point of FA project.<br/>
+ * This dummy implementation works with a simple file reader.
+ * 
+ * @author uvachon
+ */
+public class Main {
+   private static final Log LOG = LogFactory.getLog(Main.class);
+   private File source = null;
+
+   public Main(String file) {
+      source = new File(file);
+
+      if (!source.exists()) {
+         throw new RuntimeException("Unable to find the source file, aborting!");
+      }
+      LOG.info("The file: + " + source.getName() + "of entry is ready to be parse");
    }
 
+   /**
+    * 
+    */
+   public void doJob() {
+      // building raw entries list
+      Collection<RawEntry> rawEntries = getRawEntries();
+      if (CollectionUtils.isEmpty(rawEntries)) {
+         LOG.info("No correct entry found");
+         return;
+      }
+      // building items list
+      Collection<Item> items = getItems(rawEntries);
+      if (CollectionUtils.isEmpty(items)) {
+         LOG.info("No items found");
+         return;
+      }
+
+      LOG.info("Found " + items.size() + " item(s), it's time to compute");
+
+      TaxParser taxParser = new TaxParser();
+      for (Item item : items) {
+         taxParser.parse(item);
+      }
+   }
+
+   // returns collection of raw entries
+   private Collection<RawEntry> getRawEntries() {
+      FileReader fileReader = null;
+      Collection<RawEntry> rawEntries = null;
+
+      try {
+         fileReader = new FileReader(source);
+         rawEntries = new EntryReader().read(fileReader);
+      } catch (FileNotFoundException e) {
+         throw new RuntimeException("Unable to find the source file, aborting!");
+      } finally {
+         IOUtils.closeQuietly(fileReader);
+      }
+      return rawEntries;
+   }
+
+   // returns collection of item
+   private Collection<Item> getItems(Collection<RawEntry> rawEntries) {
+      return Collections2.filter(Collections2.<RawEntry, Item> transform(rawEntries, new ItemTransformer()), Predicates.notNull());
+   }
+
+   // private function based on class used to transform list
+   private class ItemTransformer implements Function<RawEntry, Item> {
+      RawEntryHandler rawEntryHandler = new RawEntryHandler();
+
+      @Override
+      public Item apply(RawEntry input) {
+         return rawEntryHandler.handle(input);
+      }
+   }
+
+   //
    public static void main(String[] args) {
-      new Main();
+      if (1 != args.length) {
+         System.err.println("Missing parameter, you must to give the file of entries...");
+         System.exit(-1);
+      }
+      new Main(args[0]).doJob();
    }
 }
--- src/main/java/org/uva/training/io/reader/EntryReader.java
+++ src/main/java/org/uva/training/io/reader/EntryReader.java
@@ -3,7 +3,6 @@ package org.uva.training.io.reader;
 import java.io.IOException;
 import java.io.Reader;
 import java.util.Collection;
-import java.util.List;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.logging.Log;
@@ -14,20 +13,30 @@ import org.uva.training.utils.RawEntryBuilder;
 import com.google.common.base.Function;
 import com.google.common.base.Predicates;
 import com.google.common.collect.Collections2;
-import com.google.common.collect.Lists;
 
+/**
+ * This class aims to
+ * 
+ * @author uvachon
+ */
 public class EntryReader {
    private static final Log LOG = LogFactory.getLog(EntryReader.class);
 
+   /**
+    * Read entries by any reader and returns a collection filled with not null elements.
+    * 
+    * @param reader the reader input stream.
+    * @return a collection filled with not null elements.
+    */
    public Collection<RawEntry> read(Reader reader) {
-      List<String> entriesByString = null;
+      Collection<String> entriesByString = null;
       try {
          entriesByString = IOUtils.readLines(reader);
       } catch (IOException e) {
          LOG.error("Unable to read the entries from reader, aborting!");
          throw new RuntimeException(e);
       }
-      return Collections2.filter(Lists.<String, RawEntry> transform(entriesByString, new RawEntryTransformer()), Predicates.notNull());
+      return Collections2.filter(Collections2.<String, RawEntry> transform(entriesByString, new RawEntryTransformer()), Predicates.notNull());
    }
 
    // private function based on class used to transform list
